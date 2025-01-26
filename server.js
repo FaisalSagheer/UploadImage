088aebd60a9f3698bd924e9a8278664ce1cc9343
Faisal <faisalsagheerqureshi@gmail.com>
2025-01-27 02:58:58 +0500

    changes

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.post('/imgUpload', upload.single('photos'), (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
-    console.log('Server running on http://localhost:3000');
+    console.log('Server running on http://localhost:8000');
 });
